5b027234c3aab603b29f961ab13612b05261ee33
정윤서 <128447702+ys0626@users.noreply.github.com>
2024-08-01 17:26:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+ 
+int main()
+{
+    fast_io
+    char a;
+    cin >> a;
+    cout << int(a);
+    return 0;
+}
