63c3d921ebe44ef1602dd02ec3b53d73692968e4
정윤서 <128447702+ys0626@users.noreply.github.com>
2024-08-06 10:49:48 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+int main()
+{
+    fast_io
+    string s;
+    while (getline(cin, s)) cout << s << endl;
+    return 0;
+}
